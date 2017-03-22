da555dd2dc6ec861f73f92f9c60c156c7ec2b5de
stdweird <stijn.deweirdt@ugent.be>
2017-03-22 07:47:12 +0100

    Configuration: reintroduce placeholder EDG::WP4::CCM::Configuration mainly for pre 1.51 maven-tools

---
+++ src/main/perl/Configuration.pm
@@ -0,0 +1,7 @@
+#${PMpre} EDG::WP4::CCM::Configuration${PMpost}
+
+warn "Direct usage of EDG::WP4::CCM::Configuration is deprecated. ",
+    "This is an empty, useless module, ",
+    "mainly to workaround improper usage in old maven-tools";
+
+1;
