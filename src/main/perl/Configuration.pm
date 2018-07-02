1fec38ec5176e7920463e72c25aeb186ed07bbb4
James Adams <james.adams@stfc.ac.uk>
2018-07-02 17:09:39 +0100

    CCM::Configuration: remove placeholder
    All repositories should now have moved to a recent maven-tools release.
    
    Resolves #169.

--- src/main/perl/Configuration.pm
+++
@@ -1,7 +0,0 @@
-#${PMpre} EDG::WP4::CCM::Configuration${PMpost}
-
-# Direct usage of EDG::WP4::CCM::Configuration is deprecated.
-# This is mainly to work around improper usage in old maven-tools.
-# Cannot warn here, as it will trigger failed warn with recent maven-tools.
-
-1;
